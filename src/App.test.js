4c0ff307cb625f977032783b7121ae15edf907a9
Yuri Tkachenko <yuri@ya.ru>
2017-11-09 20:34:00 +0200

    fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,4 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-
-  throw new Error('because fuck you thats why!');
 });
